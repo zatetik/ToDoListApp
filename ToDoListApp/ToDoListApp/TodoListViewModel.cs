1a4a91443ca90fddd6447205e5fdf937b567cbe1
zatetik <91096069+zatetik@users.noreply.github.com>
2022-05-03 16:42:37 -0400

    implemented command to add items

--- ToDoListApp/ToDoListApp/TodoListViewModel.cs
+++ ToDoListApp/ToDoListApp/TodoListViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ToDoListApp
 {
@@ -14,6 +16,13 @@ namespace ToDoListApp
             TodoItems.Add(new TodoItem("todo 1", false));
             TodoItems.Add(new TodoItem("todo 2", false));
             TodoItems.Add(new TodoItem("todo 3", false));
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+        void AddTodoItem()
+        {
+            Console.WriteLine(NewTodoInputValue);
         }
     }
 }
